539a99227d637895a6fc22b65e611cd8b142d3e9
Vikas kumar <vikask78966@gmail.com>
2026-06-25 17:08:30 +0530

    Added new class work program

---
+++ Class work/ presentation 25_june/differnce of two number.py
@@ -0,0 +1,9 @@
+'''To find out the difference of two numbers'''
+num1 = 10
+num2 = 5 
+#calculating the differnce of two numbers
+differnce = num1 -num2
+#display the diffence of two numbers
+print("The differnce of two number is:", differnce)
+'''output:
+The diffence of two number is: 5'''
